784ae2d38d644c4af9b8371229e3aabec02c1e37
Peter Oliha <poliha2002@yahoo.com>
2017-10-22 23:10:16 +0100

    updates and readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Mock Callback Server
+
+Implements callback endpoints for  [Bridge](https://github.com/stellar/bridge-server/blob/master/readme_bridge.md) and   [Compliance](https://github.com/stellar/bridge-server/blob/master/readme_compliance.md) servers. This helps you to see how the requests are senct and how you should respond accordingly
--- routes.js
+++ routes.js
@@ -3,20 +3,43 @@ module.exports = {
 
     // Bridge Callbacks
     app.post('/receive/',function(req, res) {
-      console.log("------\n");
+      console.log("---RECEIVE---\n");
       console.log(req.body);
       return res.status(200).send("Ok");
     });
 
     app.post('/error/',function(req, res) {
-      console.log("------\n");
+      console.log("---ERROR---\n");
       console.log(req.body);
       return res.status(200).send("Ok");
     });
 
     // Compliance Tx Status Callback
+    app.post('/sanctions/',function(req, res) {
+      console.log("---SANCTIONS---\n");
+      console.log(req.body);
+      return res.status(200).send("Ok");
+    });
+
+    app.post('/ask_user/',function(req, res) {
+      console.log("---ASK_USER---\n");
+      console.log(req.body);
+      return res.status(200).send("Ok");
+    });
+
+    app.post('/fetch_info/',function(req, res) {
+      console.log("---FETCH_INFO---\n");
+      console.log(req.body);
+      var customer_data = {
+      	"name": "John Doe",
+      	"address": "ce60::a4dc:c6ff:fe70:7aca",
+      	"date_of_birth": "1999-12-31"
+      };
+      return res.status(200).send(customer_data);
+    });
+
     app.get('/tx_status/',function(req, res) {
-      console.log("------\n");
+      console.log("---TX_STATUS---\n");
       console.log(req.query);
       var rp = {	"status": "status code see below",
                "recv_code": "arbitrary string",
